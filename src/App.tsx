cb1d3dae0eb2a588d5d67cb6e009e63d5c33f987
Vasyl Zuziak <zvasylvv@gmail.com>
2024-06-04 16:53:05 -0700

    chore: do smart bundling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { SubmitHandler, useForm, useFieldArray } from "react-hook-form"
+import now from '~build/time';
+import { sha } from '~build/git';
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import ProductSubform from './ProductSubform'
 import Stats from './Stats'
 import { calcSummary } from './calc-summary'
 import { FormData, Results, Product } from './interfaces'
+
+const attrName = 'data-buildmetadata';
+const attrValue = sha.slice(0, 8) + ':' + now.toISOString();
 
 const getNewProductStub = (index = 0): Product => {
   return {
@@ -32,6 +37,10 @@ function App() {
   const addProductHandler = () => {
     append(getNewProductStub(fields.length))
   }
+
+  useEffect(() => {
+    document.head.setAttribute(attrName, attrValue)
+  }, [])
 
   return (
     <>
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,32 @@
 import path from "path"
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+import Info from 'unplugin-info/vite';
+import posthtml from '@vituum/vite-plugin-posthtml'
+import { inlineFavicon } from 'posthtml-inline-favicon';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  const plugins = [react(), Info(),];
+
+  if (command === 'build') {
+    plugins.push(
+      viteSingleFile({ removeViteModuleLoader: true }) as PluginOption[],
+      posthtml({
+        plugins: [
+          inlineFavicon()
+        ]
+      })
+    )
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  }
 })
